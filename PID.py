256a9eb9b33d98ebe418dec64ffa2d261c525a54
Nishanth Hegde <hegde.nishanth@gmail.com>
2018-09-26 19:43:56 -0700

    Add comments

--- PID.py
+++ PID.py
@@ -1,10 +1,15 @@
 class PID():
-	
+	"""
+	Class PID: To store the various P, I, D values to calculate the PID output for given key
+	Author: Nishanth S Hegde
+	"""
 	def __init__(self):
 		self.PIDSystems = Dict()
 
 	@staticmethod
 	def set(key, P, I, D, kErr = 0, iErr = 0):
+		# Set P, I, D values to the given key
+		# If kErr and iErr are specified, find the key and update the corresponding values.
 		if key not in self.PIDSystems:
 			self.PIDSystems[key] = [P, I, D, kErr, iErr]
 		else:
@@ -14,6 +19,8 @@ class PID():
 
 	@staticmethod
 	def clear(key):
+		# If the key is not present in the dictionary, then set everything to zero
+		# If the key exists, then reset their error values
 		if key not in self.PIDSystems:
 			set(key, 0, 0, 0)
 		else:
@@ -22,6 +29,8 @@ class PID():
 
 	@staticmethod
 	def get(key, error, P=None, I=None, D=None):
+		# If the key is specified with P, I, D values, then set the values
+		# Else return the calculated the PID output  
 		if P not None and I not None and D not None:
 			set(key, P, I, D)
 						
